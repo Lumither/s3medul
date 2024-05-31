a1f8b19d76e5c53eaa4fa3a15b14aeb4d4c844ad
Lumither Tao <lumither@outlook.com>
2024-05-31 14:38:05 +0800

    feat(global) regular update

--- main.sh
+++ main.sh
@@ -30,38 +30,50 @@ for pic in "${orig_pics[@]}"; do
 done
 
 # compress img
-mkdir -p "$tmp_work_dir/cmprs"
-cmprs_pics=()
+mkdir -p "$tmp_work_dir/compr"
+compr_pics=()
 for pic in "${cvt_pics[@]}"; do
   raw_pic_name="$(basename "$pic")"
-  new_png_path="${tmp_work_dir}/cmprs/${raw_pic_name}"
+  new_png_path="${tmp_work_dir}/compr/${raw_pic_name}"
   new_webp_path="${new_png_path%.*}.webp"
   pngquant --speed 1 "$pic" --output "$new_png_path"
   if [ $? -eq 0 ]; then
-    cmprs_pics+=("$new_png_path")
+    compr_pics+=("$new_png_path")
   fi
   cwebp "$pic" -o "$new_webp_path" -quiet
   if [ $? -eq 0 ]; then
-    cmprs_pics+=("$new_webp_path")
+    compr_pics+=("$new_webp_path")
   fi
 done
 
 # push file to s3 storage
-for pic in "${cmprs_pics[@]}"; do
-  remote_addr="$S3ENDPOINT"
-  resource="/${S3BUCKET}/pictures/$(basename "$pic")"
+function s3upload() {
+  file="$1"
+  filepath="$2"
+
+  S3ENDPOINT="$3"
+  S3BUCKET="$4"
+  S3KEY="$5"
+  S3SECRET="$6"
+
+  resource="/${S3BUCKET}/${filepath}"
   content_type="application/octet-stream"
   date=$(date -R)
   _signature="PUT\n\n${content_type}\n${date}\n${resource}"
   signature=$(echo -en "${_signature}" | openssl sha1 -hmac "${S3SECRET}" -binary | base64)
-  curl -X PUT -T "${pic}" \
+  curl -X PUT -T "${file}" \
        -H "Host: ${S3ENDPOINT}" \
        -H "Date: ${date}" \
        -H "Content-Type: ${content_type}" \
        -H "Authorization: AWS ${S3KEY}:${signature}" \
-       "https://${remote_addr}${resource}"
-done
+       "https://${S3ENDPOINT}${resource}"
+}
 
-#res=$(convert_image "$@")
-#echo "$res"
-#compress_image "$1"
+# push orig img
+for pic in "${orig_pics[@]}"; do
+  s3upload "$pic" "pictures/orig/$(basename "$pic")" "$S3ENDPOINT" "$S3BUCKET" "$S3KEY" "$S3SECRET"
+done
+# push compressed img
+for pic in "${compr_pics[@]}"; do
+  s3upload "$pic" "pictures/compr/$(basename "$pic")" "$S3ENDPOINT" "$S3BUCKET" "$S3KEY" "$S3SECRET"
+done
